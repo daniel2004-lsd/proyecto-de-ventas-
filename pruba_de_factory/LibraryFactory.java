f49af7ec211f66ac01cc64516a32c44c8d55ce38
daniel <danielsalazarvargas953@gmail.com>
2025-12-10 23:09:58 -0500

    Limpieza total de rama main

--- pruba_de_factory/LibraryFactory.java
+++
@@ -1,9 +0,0 @@
-package ejercicios_carlos_julio.pruba_de_factory;
-
-//define metodos que debe tener cualquier fabrica 
-public interface LibraryFactory {
-    Book createBook(String title, String author, String genre, String isbn);
-    Reader createReader(String name, String lastName, String carnet_cedula, String gmail);
-    Loan createLoan(Book book, Reader reader);
-    Library createLibrary();
-}
